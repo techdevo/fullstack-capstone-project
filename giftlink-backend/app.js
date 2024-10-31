40fabd3caf3dd52672c36326b9c60c125b842af1
techdevo <iiit.upendra@gmail.com>
2024-10-31 16:58:37 -0400

    linting issue fixing

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -41,9 +41,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
