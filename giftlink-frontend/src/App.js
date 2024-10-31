d916ee37ea7f1894be407acf788b0696536bb67b
techdevo <iiit.upendra@gmail.com>
2024-10-31 13:57:42 -0400

    Registration page added

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,8 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
